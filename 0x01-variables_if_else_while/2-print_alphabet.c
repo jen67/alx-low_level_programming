f37e58ab909d6be376ed9b31bf01daeeac22468e
Gift Amachree <amakrigift2000@gmail.com>
2022-09-10 02:58:59 -0700

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,32 +1,19 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
  * main - print if the number is positive, zero, or negative
  *
  * Description: using the main function
- * this program prints "programming is positive, zero, or negative
+ * this program prints "Programming is posivive, zero, or negative
  * Return: 0
  */
 int main(void)
 {
-int n;
-
-srand(time(0));
-n = rand() - RAND_MAX / 2;
- /* your code goes there */
-if (n > 0)
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
 {
-	printf("%i is positive\n", n);
+	putchar(ch);
 }
-else if (n == 0)
-{
-        printf("%i is zero\n", n);
-}
-else if (n < 0)
-{
-	printf("%i is negative\n", n);
-}
+putchar('\n');
 return (0);
 }
 
