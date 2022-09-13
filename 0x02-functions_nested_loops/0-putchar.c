3d766c274e3644c37e5a6f336a066e26f871c218
Gift Amachree <amakrigift2000@gmail.com>
2022-09-13 07:34:10 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,21 @@
 #include "main.h"
 
-void 0-putchar.c
+/**
+ * main - print putchar
+ * Return: always 0
+ */
+
+int main(void)
 {
-	write your function here
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
