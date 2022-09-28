992625d057801583f981043ce44e4d4ccd960b8b
Gift Amachree <amakrigift2000@gmail.com>
2022-09-28 08:04:53 -0700

    5. Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,37 +1,30 @@
-#include "main.h"
+#include <main.h>
 
 /**
- * _evaluate - evaluate function sqrt
- * @i: interger
- * @n: interger
- * Return: evaluate sqrt
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to be used
+ *
+ * Return: the square root of n
  */
-
-int _evaluate(int i, int n)
-{
-	/*Evaluate function*/
-	if (n == 0 || n == 1)
-		return (n);
-	else if (i * i < n)
-		return (_evaluate(i + 1, n));
-	else if (i * i == n) /*Condiction base*/
-		return (i);
-	return (-1);
-	return (-1);
-}
-/**
- * _sqrt_recursion - evaluate sqrt
- *  @n: interger
- *   Return: Sgrt_recursion
- */
-
 int _sqrt_recursion(int n)
 {
-	int i = 0;
+	if (n == 1 || n == 0)
+		return (n);
+	return (_sqrt(0, n));
+}
 
-	if (i < 0) /*If n is negative*/
+/**
+ * _sqrt - returns the square root of a number
+ * @n: test the number
+ * @x: squared number
+ *
+ * Reeturn: the square root of n
+ */
+int _sqrt(int n, int x)
+{
+	if (n > x / 2)
 		return (-1);
-	else if
-	{return (_evaluate(i, n)); /*Recursive call*/
-	}
+	else if (n * n == x)
+		return (n);
+	return (_sqrt(n + 1, x));
 }
