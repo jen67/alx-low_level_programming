1478225fc01498fe5d7b231d2eb4dae58a90c5d7
Gift Amachree <amakrigift2000@gmail.com>
2022-09-30 10:50:30 -0700

    3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,8 +5,8 @@
 /**
  * main - prints the multiplication of two integers
  * @argc: argument count
- * @argv: argument vector
- * Return: 0 if true, 1 if false
+ *  @argv: argument vector
+ *  Return: 0 if true, 1 if false
  */
 int main(int argc, char *argv[])
 {
