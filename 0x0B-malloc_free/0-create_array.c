e2e501ab87fdb6715151ed8fc1522802fb894431
Gift Amachree <amakrigift2000@gmail.com>
2022-10-04 15:09:39 -0700

    0. Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,25 +1,31 @@
 #include "main.h"
-/**
- * create_array - array for prints a string
- * size: number elements array
- * @c: char
- * Return: pointer
- */
+#include <stdlib.h>
 
+/**
+ *  create_array - creates an array of chars
+ *  @size: The size of the array
+ *   @c: The char to fill in the array
+ *
+ *   Return: The array filled
+ */
 char *create_array(unsigned int size, char c)
 {
-	char *array;
-	unsigned int index;
+	unsigned int i;
+	char *s;
 
 	if (size == 0)
 		return (NULL);
-	array = malloc(sizeof(char) *size);
 
-	if (array == NULL)
+	s = malloc(size * sizeof(char));
+
+	if (s == NULL)
 		return (NULL);
 
-	for (index = 0; index < size; index++)
-		array[index] = c;
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
+	}
 
-	return (array);
+	return (s);
 }
+
