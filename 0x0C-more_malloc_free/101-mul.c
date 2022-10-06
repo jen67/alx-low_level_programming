b8641b0bb8de0bfa6608f628fc351e57d771858c
Gift Amachree <amakrigift2000@gmail.com>
2022-10-06 14:10:26 -0700

    5. We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 /**
  * _print - moves a string one place to the left and prints the string
- *  @str: string to move
- *  @l: size of string
- *  Return: void
+ * @str: string to move
+ * @l: size of string
+ * Return: void
  */
 void _print(char *str, int l)
 {
@@ -28,9 +28,10 @@ void _print(char *str, int l)
  * mul - multiplies a char with a string and places the answer into dest
  * @n: char to multiply
  * @num: string to multiply
- *  @num_index: last non NULL index of num
- *  @dest: destination of multiplication
- *  @dest_index: highest index to start addition
+ * @num_index: last non NULL index of num
+ * @dest: destination of multiplication
+ * @dest_index: highest index to start addition
+ * Return: pointer to dest, or NULL on failure
  */
 char *mul(char n, char *num, int num_index, char *dest, int dest_index)
 {
@@ -96,8 +97,10 @@ void init(char *str, int l)
  * main - multiply two numbers
  * @argc: number of arguments
  * @argv: argument vector
+ *
  * Return: zero, or exit status of 98 if failure
  */
+
 int main(int argc, char *argv[])
 {
 	int l1, l2, ln, ti, i;
@@ -137,3 +140,4 @@ int main(int argc, char *argv[])
 	_print(a, ln - 1);
 	return (0);
 }
+
