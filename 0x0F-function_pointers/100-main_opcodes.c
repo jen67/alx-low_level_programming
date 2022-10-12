55a70bce3b4c9610211a0c63e7048532f4ae0188
Gift Amachree <amakrigift2000@gmail.com>
2022-10-12 14:38:14 -0700

    4. Most hackers are young because young people tend to be adaptable. As long as you remain adaptable, you can always be a good hacker

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,39 +1,48 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- *  main - a program that prints the opcodes of its own main function.
- *  @argc: The number of arguments passed to the program.
- *  @argv: The argument list.
- *  Return: 0 on success, or exit on failure.
+ * print_opcodes - print the opcodes of this program
+ * @a: address of the main function
+ * @n: number of bytes to print
+ *
+ * Return: void
  */
-int main(int argc, char *argv[])
+void print_opcodes(char *a, int n)
 {
-	unsigned char *f;
-	int bytes, i;
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%.2hhx", a[i]);
+		if (i < n - 1)
+			printf(" ");
+	}
+	printf("\n");
+}
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: number of arguments passed to the function
+ * @argv: array of pointers to arguments
+ *
+ * Return: always O
+ */
+int main(int argc, char **argv)
+{
+	int n;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-
-	bytes = atoi(argv[1]);
-	if (bytes < 0)
+	n = atoi(argv[1]);
+	if (n < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-
-	f = (unsigned char *)main;
-	for (i = 0; i < bytes; i++)
-	{
-		printf("%02x", *(f + i));
-		if (i < bytes - 1)
-			printf(" ");
-		printf(" ");
-	}
-	printf("\n");
-
+	print_opcodes((char *)&main, n);
 	return (0);
 }
